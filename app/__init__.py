2726ed7d15191862b959a3ec9688fd909511c324
System256 <system256@ya.ru>
2023-06-21 07:48:31 +0300

    Заполнение полей для записи в БД

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from app.forms import AddRecipeForm
 from app.model import db, Recipe
 
@@ -9,7 +9,6 @@ def create_app():
     db.init_app(app)
 
     @app.route('/')
-    @app.route('/home')
     def index():
         # Нужно доделать вывод автора рецепта, дату и т.п.
         recipes_list = Recipe.query.order_by(Recipe.created_at.desc()).all()
@@ -30,10 +29,19 @@ def create_app():
     @app.route('/process-add-recipe', methods=['POST'])
     def process_add_recipe():
         form = AddRecipeForm()
-
-        # if form.validate_on_submit():
-            
-
+        if form.validate_on_submit():
+            new_recipe = Recipe(title = form.title.data,
+                                 decription_recipe = form.decription_recipe.data,
+                                 steps_recipe = form.steps_recipe.data,
+                                 servings = form.servings.data,
+                                 time_cooking = form.time_cooking.data
+                                 )
+            db.session.add(new_recipe)
+            db.session.commit()
+            flash('Рецепт успешно добавлен!')
+            return redirect(url_for('index'))
+        flash('Неправильно заполнена форма!')
+        return redirect(url_for('add_recipe'))
     
 
     return app
