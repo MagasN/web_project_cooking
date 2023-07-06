934dc587dc0361327ea80d97cdb372b92274b3f4
System256 <system256@ya.ru>
2023-07-06 22:44:10 +0300

    мелкая правка адреса

--- app/admin/views.py
+++ app/admin/views.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager, current_user, login_required
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
 
-@blueprint.route('/admin')
+@blueprint.route('/')
 @login_required
 def admin_index():
     if current_user.is_admin:
